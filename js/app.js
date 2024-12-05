ba5c349e6d48a1818bc5e50bcc9ccc9e1bdc3878
yasin malekian <yasinmalekian90@gmail.com>
2024-12-05 23:55:08 +0330

    adding first coomit no storage

---
+++ js/app.js
@@ -0,0 +1,71 @@
+const modal = document.querySelector('.modal')
+const addBtn = document.querySelector('#add_btn')
+const overlay = document.querySelector('#overlay')
+const closeModal = document.querySelector('.close-modal')
+const submitNewTodo = document.querySelector('#todo_submit') 
+const todoInput = document.querySelector('#todo_input')
+const noStatusArea = document.querySelector('#no_status')
+const StatusArea = document.querySelectorAll('.status')
+let todoCounter = 0
+//* events
+addBtn.addEventListener('click', modalActions)
+closeModal.addEventListener('click', modalActions)    // event of open and close modal with click
+overlay.addEventListener('click', modalActions)
+//! ...............................................
+submitNewTodo.addEventListener('click', submitModal)
+noStatusArea.addEventListener('click', closeTodoActive)
+StatusArea.forEach((arae)=>{
+    arae.addEventListener('dragstart', drapHandeler)
+    arae.addEventListener('dragover', (event)=> event.preventDefault())
+    arae.addEventListener('drop', dropHandeler)
+})
+
+function modalActions(){
+    modal.classList.toggle('active');                   // function of open and close modal with click
+    overlay.classList.toggle('active')  
+}
+function submitModal(){
+    createNewStatus()
+    modalActions()
+    todoInput.value = ''
+}
+function createNewStatus(){
+    let todoText = todoInput.value
+    const newTodoCreate = document.createElement('div')
+    newTodoCreate.setAttribute('draggable' , 'true')
+    newTodoCreate.classList.add('todo')
+    const uniqueId = `todo-${todoCounter}`;
+    todoCounter++
+    newTodoCreate.setAttribute('id', uniqueId);
+    newTodoCreate.innerHTML = `
+    ${todoText}
+    <span class="close">&times;</span>
+    `;
+    noStatusArea.appendChild(newTodoCreate)
+}
+function closeTodoActive(event){
+    if(event.target.classList.contains('close')){
+        let parentElm = event.target.parentNode
+        parentElm.remove()
+    }
+}
+/////////////////////////////////////////////////////////////
+document.querySelectorAll('.todo').forEach((todo) => {
+    const uniqueId = `todo-${todoCounter}`;
+    todo.setAttribute('id', uniqueId);
+    todoCounter++; 
+});
+function drapHandeler(ev) { 
+    ev.dataTransfer.setData('elemDataID', ev.target.getAttribute('id'))
+}
+function dropHandeler(ev){
+    ev.preventDefault()
+    const elmDataId = ev.dataTransfer.getData('elemDataID')
+    let targetElm = document.getElementById(elmDataId)
+    document.getElementById(ev.toElement.id).appendChild(targetElm)
+}
+
+
+
+
+
